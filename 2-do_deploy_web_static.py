8ee932ae14bbdd4ed9581a808723cf7f61641dbb
hafsa chenni <hafsachenni77@gmail.com>
2024-03-07 12:52:26 +0100

    fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers"""
-
+"""this a fabric that distributes a compressed
+archive to both of my web servers"""
 from fabric.api import put, run, env
 from os.path import exists
+
 env.hosts = ['54.167.198.176', '35.175.102.14']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """deploying the compressed archive to my servers"""
     if exists(archive_path) is False:
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception as err:
-        return False
+
+    nameof_file = archive_path.split('/')[-1]
+    filename = nameof_file.split('.')[0]
+
+    put(archive_path, "/tmp/")
+    run("mkdir -p /data/web_static/releases/{}/"
+        .format(filename))
+    run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+        .format(nameof_file, filename))
+    run("rm /tmp/{}".format(nameof_file))
+    run("mv /data/web_static/releases/{}/web_static/* "
+        "/data/web_static/releases/{}/"
+        .format(filename, filename))
+    run("rm -rf /data/web_static/releases/{}/web_static"
+        .format(filename))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(filename))
+
+    print("New version deployed!")
+    return True
